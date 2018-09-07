8d10a6bc6df4412ffb57dd1f1453f0d66e895bea
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-09-08 01:31:08 +0530

    accessing the form via ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,16 +7,27 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f')
+  form: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
-  onSubmit(form: NgForm) {
-    console.log(form);
-    // Will be false if no input is added
-    console.log(form.dirty);
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  //   // Will be false if no input is added
+  //   console.log(form.dirty);
 
-    // Form value based on name we set as attribute
-    console.log(form.value);
+  //   // Form value based on name we set as attribute
+  //   console.log(form.value);
+  // }
+
+  onSubmit() {
+    console.log(this.form);
+      // Will be false if no input is added
+      console.log(this.form.dirty);
+
+      // this.form value based on name we set as attribute
+      console.log(this.form.value);
   }
 }
